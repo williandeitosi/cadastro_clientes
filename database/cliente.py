7a8c3adc057954846f709ae459c74e5dc3ec4d55
willian <williandeitosi@gmail.com>
2024-01-23 22:43:50 -0300

    first commit

---
+++ database/cliente.py
@@ -0,0 +1,7 @@
+CLIENTES = [
+  {"id": 1, "nome": "Willian", "email": "willian@email.comm"},
+  {"id": 2, "nome": "Rafaela", "email": "rafaela@email.comm"},
+  {"id": 3, "nome": "Marcos", "email": "marcos@email.comm"},
+  {"id": 4, "nome": "Maria", "email": "maria@email.comm"},
+  {"id": 5, "nome": "Jhon", "email": "jhon@email.comm"},
+]
